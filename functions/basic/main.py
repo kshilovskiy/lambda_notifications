ae80e171d4a79d693af8e04197f7378bde8846f7
Konstantin Shilovskiy <kshilovskiy@gmail.com>
2017-11-05 14:58:06 +0100

    Add logging

--- functions/basic/main.py
+++ functions/basic/main.py
@@ -1,12 +1,19 @@
 import os
+import logging
 import notifier
 import event_parser
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 def handle(s3_event, context):
     receiver = _get_receiver()
 
     for event in s3_event['Records']:
         s3_path = event_parser.extract_s3_path(event)
+        logger.info(f"Extracted path {s3_path}")
+
         notifier.notify(receiver, s3_path)
 
 def _get_receiver():
--- functions/basic/notifier.py
+++ functions/basic/notifier.py
@@ -1,7 +1,13 @@
 import requests
+import logging
+
+logger = logging.getLogger()
 
 def notify(receiver, s3_path):
     request = _build_request(receiver, s3_path)
+
+    logger.info(f"Recepient: {request['url']}\nBody: {request['body']}")
+
     requests.post(request['url'], json=request['body'], auth=request['auth'])
 
 def _build_request(receiver, s3_path):
